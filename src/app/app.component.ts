3f990cc056a350ccc81b079061248ee968951f5d
ArkBog <arkadiuszbogdanski1@gmail.com>
2024-08-13 21:39:00 +0200

    added changeStatus method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, HomeComponent, NavbarComponent, FooterComponent],
+  imports: [RouterLink, RouterOutlet, HomeComponent, NavbarComponent, FooterComponent,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Seat, Table } from '../../../interfaces';
 import { ApiService } from '../../services/api.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-book',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss'
 })
@@ -14,5 +15,29 @@ export class BookComponent {
   constructor(private apiService:ApiService) {}
 
   tables:Table[] = this.apiService.tables;
+
+  choosenTables:number[]=[];
+
+  tableForm = new FormGroup({
+    table: new FormControl('')
+  });
+
+  onSubmit(){
+    console.log(this.tableForm)
+  };
+
+  changeStatus($event:any, tableID: number){
+    if($event.target.checked === true){
+      this.choosenTables.push(tableID);
+      // console.log(this.choosenTables)
+    } else {
+     const currentIndex =  this.choosenTables.findIndex((e) => e === tableID);
+     console.log(currentIndex)
+     const newChoosenTables = this.choosenTables.filter((_, index) => index !== currentIndex);
+     console.log(newChoosenTables)
+    }
+    console.log($event.target.checked)
+  };
+
 
 }
