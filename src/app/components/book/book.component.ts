7f326e81f1cc775803a207c53c693814239de41d
ArkBog <arkadiuszbogdanski1@gmail.com>
2024-08-17 17:29:33 +0200

    styled book and fixed onSubmit function

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Seat, Table } from '../../../interfaces';
 import { ApiService } from '../../services/api.service';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-book',
@@ -12,7 +13,7 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class BookComponent {
 
-  constructor(private apiService:ApiService) {}
+  constructor(private apiService:ApiService, private cartService: CartService) {}
 
   tables:Table[] = this.apiService.tables;
 
@@ -23,20 +24,18 @@ export class BookComponent {
   });
 
   onSubmit(){
-    console.log(this.tableForm)
+    this.cartService.tablesId = this.choosenTables;
+    console.log(this.cartService.tablesId);
   };
 
   changeStatus($event:any, tableID: number){
     if($event.target.checked === true){
       this.choosenTables.push(tableID);
-      // console.log(this.choosenTables)
     } else {
      const currentIndex =  this.choosenTables.findIndex((e) => e === tableID);
-     console.log(currentIndex)
      const newChoosenTables = this.choosenTables.filter((_, index) => index !== currentIndex);
-     console.log(newChoosenTables)
+     this.choosenTables = newChoosenTables
     }
-    console.log($event.target.checked)
   };
 
 
